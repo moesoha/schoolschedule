738634f7eeb65f154a2502d7a05e55e45a796ed9
Soha Jin <soha@lohu.info>
2021-01-12 01:40:08 +0800

    schedule view

--- app/src/main/java/dev/soha/course202001/schoolschedule/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/dev/soha/course202001/schoolschedule/ui/schedule/ScheduleFragment.kt
@@ -1,21 +1,76 @@
 package dev.soha.course202001.schoolschedule.ui.schedule
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.GridLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import dev.soha.course202001.schoolschedule.R
+import dev.soha.course202001.schoolschedule.ui.lesson.LessonShowActivity
 
 class ScheduleFragment: Fragment() {
+	companion object {
+		val TAG = ScheduleFragment::class.qualifiedName
+	}
+
 	private lateinit var scheduleViewModel: ScheduleViewModel
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
 	): View? {
-		scheduleViewModel = ViewModelProvider(this).get(ScheduleViewModel::class.java)
+		scheduleViewModel = ViewModelProvider(this).get(ScheduleViewModel::class.java).apply {
+			var isFirstObserved = true
+			displayingWeek.observe(viewLifecycleOwner) {
+				activity?.actionBar?.title = getString(R.string.week_n, it)
+				if (isFirstObserved) {
+					isFirstObserved = false
+					return@observe
+				}
+				updateScheduleLayout()
+			}
+			lessons.observe(viewLifecycleOwner) {
+				updateScheduleLayout()
+			}
+		}
 		val root = inflater.inflate(R.layout.fragment_schedule, container, false)
 		return root
+	}
+
+	fun updateScheduleLayout() {
+		Log.v("$TAG::updateScheduleLayout", "checking")
+		val grid: GridLayout = activity?.findViewById(R.id.grid) ?: return
+		val lessons = scheduleViewModel.lessons.value ?: return
+
+		Log.v("$TAG::updateScheduleLayout", "started")
+		val usedSpace = Array(7) { Array(12) { false } }
+		for (lesson in lessons) {
+			val button = Button(context).apply {
+				layoutParams = GridLayout.LayoutParams(
+					GridLayout.spec(lesson.session.start, lesson.session.span, 1f),
+					GridLayout.spec(lesson.day.value, 1, 1f),
+				).apply {
+					height = 0
+					width = 0
+				}
+				text = lesson.name
+				setOnClickListener {
+					startActivity(
+						Intent(activity, LessonShowActivity::class.java)
+							.putExtra(LessonShowActivity.INTENT_EXTRA_ID, lesson.id)
+					)
+				}
+			}
+			grid.addView(button)
+			for (i in lesson.session.start..lesson.session.end) {
+				usedSpace[lesson.day.value - 1][i - 1] = true
+			}
+		}
+
+		Log.v("$TAG::updateScheduleLayout", "finished")
 	}
 }
--- app/src/main/java/dev/soha/course202001/schoolschedule/ui/schedule/ScheduleViewModel.kt
+++ app/src/main/java/dev/soha/course202001/schoolschedule/ui/schedule/ScheduleViewModel.kt
@@ -1,12 +1,23 @@
 package dev.soha.course202001.schoolschedule.ui.schedule
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
+import dev.soha.course202001.schoolschedule.model.Lesson
+import dev.soha.course202001.schoolschedule.repository.LessonRepository
+import dev.soha.course202001.schoolschedule.repository.SettingRepository
+import kotlinx.coroutines.launch
 
-class ScheduleViewModel: ViewModel() {
-	private val _text = MutableLiveData<String>().apply {
-		value = "This is dashboard Fragment"
+class ScheduleViewModel(application: Application): AndroidViewModel(application) {
+	private val lessonRepository = LessonRepository(application)
+	private val settingRepository = SettingRepository(application)
+	val lessons: LiveData<List<Lesson>> = lessonRepository.getAllLessons()
+
+	private val _displayingWeek: MutableLiveData<Int> = MutableLiveData(1)
+	val displayingWeek: LiveData<Int> = _displayingWeek
+
+	init {
+		viewModelScope.launch {
+			_displayingWeek.value = settingRepository.getCurrentRelativeWeekNumber()
+		}
 	}
-	val text: LiveData<String> = _text
 }
