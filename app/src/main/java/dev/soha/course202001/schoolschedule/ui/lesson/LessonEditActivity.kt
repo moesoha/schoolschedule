e875b9c02d865a5ac84e0e7044e898b09ee25013
Soha Jin <soha@lohu.info>
2021-01-12 15:28:10 +0800

    add

---
+++ app/src/main/java/dev/soha/course202001/schoolschedule/ui/lesson/LessonEditActivity.kt
@@ -0,0 +1,12 @@
+package dev.soha.course202001.schoolschedule.ui.lesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.soha.course202001.schoolschedule.R
+
+class LessonEditActivity: AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_lesson_edit)
+	}
+}
