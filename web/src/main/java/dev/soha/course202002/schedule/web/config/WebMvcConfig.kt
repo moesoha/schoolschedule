6b7cb4a1afb2503c74c4b0a7cb371fa0dc095a55
Soha Jin <soha@lohu.info>
2021-06-24 19:58:50 +0800

    fix bug

--- web/src/main/java/dev/soha/course202002/schedule/web/config/WebMvcConfig.kt
+++ web/src/main/java/dev/soha/course202002/schedule/web/config/WebMvcConfig.kt
@@ -3,11 +3,9 @@ package dev.soha.course202002.schedule.web.config
 import dev.soha.course202002.schedule.web.interceptor.WebSessionInterceptor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@EnableWebMvc
 @Configuration class WebMvcConfig : WebMvcConfigurer {
 	@Bean fun getWebSessionInterceptor(): WebSessionInterceptor? {
 		return WebSessionInterceptor()
