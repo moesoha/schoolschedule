d87749e4ea8cc0b1f0d43c75455e1eaa690ce6eb
Soha Jin <soha@lohu.info>
2021-06-27 16:12:51 +0800

    change launcher icon and fix bug

--- android/src/main/java/dev/soha/course202002/schedule/android/ui/MainActivity.kt
+++ android/src/main/java/dev/soha/course202002/schedule/android/ui/MainActivity.kt
@@ -3,7 +3,7 @@ package dev.soha.course202002.schedule.android.ui
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -15,7 +15,7 @@ class MainActivity: AppCompatActivity() {
 		setContentView(R.layout.activity_main)
 		val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-		val navController = findNavController(R.id.nav_host_fragment)
+		val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
 		val appBarConfiguration = AppBarConfiguration(
 			setOf(R.id.navigation_today, R.id.navigation_schedule, R.id.navigation_setting)
 		)
